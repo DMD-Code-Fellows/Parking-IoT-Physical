d0189dcda64a672d82b527571006750e854adfa5
dhwang21 <hwang.derrick@gmail.com>
2018-12-28 17:54:03 -0800

    spring init

---
+++ src/main/java/com/dmd/physical_iot/physical/PhysicalApplication.java
@@ -0,0 +1,14 @@
+package com.dmd.physical_iot.physical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhysicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhysicalApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/dmd/physical_iot/physical/PhysicalApplicationTests.java
@@ -0,0 +1,17 @@
+package com.dmd.physical_iot.physical;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhysicalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
