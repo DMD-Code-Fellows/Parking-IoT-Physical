f442ce11d0dda67a2c276e8290c1c2a43cc50016
dhwang21 <hwang.derrick@gmail.com>
2019-01-04 01:47:46 -0800

    removed class files

--- src/test/java/com/dmd/physical_iot/physical/PiControllerTest.java
+++ src/test/java/com/dmd/physical_iot/physical/PiControllerTest.java
@@ -1,19 +1,8 @@
 package com.dmd.physical_iot.physical;
-import org.junit.Test;
 
-import static org.junit.Assert.*;
+
 
 public class PiControllerTest {
 
-    @Test
-    public void helloWorldTest() {
-        String expected = "hello world!";
-
-        String homeRun = PiController.home();
-
-        assertEquals(expected, homeRun);
-
-
-    }
 
 }
